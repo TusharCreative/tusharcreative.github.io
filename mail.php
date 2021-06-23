8ab14adf03561b916a75c288a9d3ff3942853fc6
TusharCreative <86317567+TusharCreative@users.noreply.github.com>
2021-06-23 03:39:01 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,13 +3,13 @@
 //get data from form
 $name= $_POST['name']};
 $email= $_POST['email']};
+$project=$_POST['project'];
 $message= $_POST['message']};
 $to="arifulhoque.tushar@gmail.com";
 $subject="Mail From Website";
-$txt="name=".$name."\r\n Email=".$email."\r\n Project=".$project."\r\n Message=".$message=".$message;
-
-$headers = "From:noreply@codeconia.com"."\r\n.
-"cc:somebodyelse@example.com";
+$txt="name=".$name."\r\n Email=".$email."\r\n Project=".$project."\r\n Message=".$message;
+$headers="From:noreply@tusharcreative.github.io"."\r\n.
+"CC:somebodyelse@example.com";
 if($email!=Null){
 	mail($to,$subject,$txt,$headers)
 }
@@ -17,4 +17,3 @@ if($email!=Null){
 header("Location:thankyou.html");
 
 ?>
-
